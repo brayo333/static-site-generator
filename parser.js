6a1733e7c138bd362108572a31d7b9f0fa43a29b
brayo333 <mulaabrian@gmail.com>
2023-02-08 16:13:09 +0300

    feat: refactoring parser.js

--- parser.js
+++ parser.js
@@ -4,6 +4,69 @@ const { rules } = require("./rules");
 
 const EXTENSION = ".md";
 
+let posts = [];
+let pages = [];
+
+// The function provides html templating based on whether the source md file was meant for a post or a page
+function HTMLTemplate(type, htmlContent) {
+  let finalContent;
+
+  switch (type) {
+    case "pages":
+      finalContent = `<!DOCTYPE html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8" />
+                            <title>This is a page</title>
+                            <script src="https://cdn.tailwindcss.com"></script>
+                          </head>
+
+                          <body>
+                            <div class="w-full p-5">
+                              ${htmlContent}
+                            </div>
+                          </body>
+                      </html>`;
+      break;
+
+    case "posts":
+      finalContent = `<!DOCTYPE html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8" />
+                            <title>This is a post</title>
+                            <script src="https://cdn.tailwindcss.com"></script>
+                          </head>
+
+                          <body>
+                            <div class="w-full p-5">
+                              ${htmlContent}
+                            </div>
+                          </body>
+                      </html>`;
+      break;
+
+    default:
+      finalContent = `<!DOCTYPE html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8" />
+                            <title>This is something</title>
+                            <script src="https://cdn.tailwindcss.com"></script>
+                          </head>
+
+                          <body>
+                            <div class="w-full p-5">
+                              ${htmlContent}
+                            </div>
+                          </body>
+                      </html>`;
+  }
+
+  return finalContent;
+}
+
+// The function adds the html content passed to it after conversion into a html file with file path specified
 function writeHTMLFile(filePath, htmlContent) {
   fs.open(filePath, "w+", function (err, fd) {
     if (err) {
@@ -21,12 +84,18 @@ function writeHTMLFile(filePath, htmlContent) {
   });
 }
 
+// The function loops through a directory/folder and looks for md files & converts the content into html content
 function loopDirectoryMDFiles(directoryName) {
   fs.readdir(directoryName, (err, files) => {
     if (err) throw err;
 
     for (let file of files) {
       if (path.parse(file).ext === EXTENSION) {
+        if (directoryName == "pages")
+          pages.push({ name: path.parse(file).name });
+        if (directoryName == "posts")
+          posts.push({ name: path.parse(file).name });
+
         fs.open(`${directoryName}/${file}`, "r", function (err, fd) {
           if (err) {
             return console.error(err);
@@ -45,20 +114,7 @@ function loopDirectoryMDFiles(directoryName) {
                 html = html.replace(rule, template);
               });
 
-              html = `<!DOCTYPE html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8" />
-                            <title>This is a page</title>
-                            <script src="https://cdn.tailwindcss.com"></script>
-                          </head>
-  
-                          <body>
-                            <div class="w-full p-5">
-                              ${html}
-                            </div>
-                          </body>
-                      </html>`;
+              html = HTMLTemplate(directoryName, html);
 
               writeHTMLFile(
                 `public/${directoryName}/${path.parse(file).name}.html`,
@@ -76,8 +132,50 @@ function loopDirectoryMDFiles(directoryName) {
   });
 }
 
+function writeIndexHTML() {
+  let pagesLink;
+  let indexFile
+
+  pages.forEach(([page]) => {
+    pagesLink = pagesLink.concat(
+     "<a href=`/${page.name}` class=`capitalize`>`${page.name}`</a>"
+    );
+  });
+
+  indexFile = `<!DOCTYPE html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8" />
+                            <title>Website made using a custom static site generator</title>
+                            <script src="https://cdn.tailwindcss.com"></script>
+                          </head>
+
+                          <body>
+                            <div class="w-full grid grid-col grid-cols-5 gap-3">
+                              <nav class="col-span-1 h-screen p-5 flex flex-col">
+                                <div class="w-full h-full flex flex-col overflow-y-auto">
+                                  <a href="/">Home</a>
+                                  ${pagesLink}
+                                </div>
+                              </nav>
+
+                              <div class="col-span-4 w-full p-5 flex flex-col">
+                                ${posts.forEach((post) => {
+                                  "<p>Hello there</p>";
+                                })}
+                              </div>
+                            </div>
+                          </body>
+                      </html>`;
+
+  writeHTMLFile("public/index.html", indexFile);
+}
+
 // Convert the posts markdown files
 loopDirectoryMDFiles("posts");
 
 // Convert the pages markdown files
 loopDirectoryMDFiles("pages");
+
+// Write the index file for the HOme page
+writeIndexHTML();
